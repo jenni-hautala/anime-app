961633dca77d4ed30bd6f8acc39ca677644a102d
Jenni Hautala <jennihau@Jennis-MacBook-Pro.local>
2024-04-09 00:00:01 +0300

    Slice init

--- src/app/containers/HomePage/homePageSlice.ts
+++ src/app/containers/HomePage/homePageSlice.ts
@@ -3,18 +3,16 @@ import { createSlice } from '@reduxjs/toolkit';
 // All the magic happens in this file.
 
 const initialState = {
-    counter: 0,
+    animeList: [],
 };
 
 const HomePageSlice = createSlice({
     name: 'homePage',
     initialState,
     reducers: {
-        incrementCounter: (state, action) => {
-            state.counter += 1;
-        }
+       
     }
 });
 
-export const { incrementCounter }  = HomePageSlice.actions;
+export const {  }  = HomePageSlice.actions;
 export default HomePageSlice.reducer;
